4a3cb57bb632dc04733a26328a398e9fccfd6366
wzc0003 <wzc0003@sina.com>
2023-01-10 11:16:46 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -56,7 +56,7 @@ const USER_CONFIG = {
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'PDU19287T43KvcwtX7VxD2k24tsaMc0Uo6Ep6XODG',
+      id: '',
     }
   ],
 

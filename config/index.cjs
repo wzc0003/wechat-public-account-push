6f8526c2e2cba995646adda9502a9fd6e7d264d8
wzc0003 <wzc0003@sina.com>
2023-01-10 11:42:47 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -22,7 +22,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ocrNF6i1ay80N9o5G51lhDvU__mY',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'l2thzDqYKyuTKiMO1DoxeTBjUmqMLcECo3i8ucZRmkg',
+      useTemplateId: 'xAdY9ZSwxEazCGYNG2WLl85jLewwWHBhdZlTVqTD6mU',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '11-23',
       festivals: [
